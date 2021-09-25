5ebea71d90f2f8db59a4cde58812f36a736af4a2
valleykid <valleykiddy@gmail.com>
2021-09-25 22:20:08 +0800

    feat: a qiankun-sandbox example (#4)

---
+++ getting-started/simple-midware-qiankun-sandbox/mock/api.ts
@@ -0,0 +1,4 @@
+export default {
+  'POST /api/**/*List': { code: 0, msg: '成功', result: [], totalNum: 0 },
+  'POST /api/**/*': { code: -1, msg: '失败' },
+};
---
+++ getting-started/simple-midware-qiankun-sandbox/src/app.ts
@@ -0,0 +1,20 @@
+import { register, start, use } from '@satumjs/core';
+import midwareQiankunSandbox from '@satumjs/midware-qiankun-sandbox';
+
+register({
+  name: 'occ-mobile',
+  entry: 'https://render.alipay.com/p/w/occ-mobile/index.html',
+  rules: {
+    rule: '/',
+    container: '#mountNode',
+  },
+});
+
+use((system, _, next) => {
+  system.set('processUrlOption', { whiteList: ['/api'] });
+  next();
+});
+
+use(midwareQiankunSandbox, { useQiankunStart: true, useLooseSandbox: true });
+
+start();
