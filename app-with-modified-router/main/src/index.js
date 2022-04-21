a19c2362a6101ec6e8eb7e0757fb3a2bc3ef13cd
valleykid <valleykiddy@gmail.com>
2022-04-21 08:22:52 +0800

    feat: a demo with modified-router

---
+++ app-with-modified-router/main/src/index.js
@@ -0,0 +1,29 @@
+// @ts-check
+import '@babel/polyfill';
+import { register, start, use, MidwareName } from '@satumjs/core';
+import theadOccMidware from '@satumjs/midware-thead-occ';
+
+register({
+  name: 'react',
+  entry: 'http://localhost:3000/',
+  rules: {
+    rule: '/',
+    container: '#mountNode'
+  },
+});
+
+use((sys, apps, next) => {
+  sys.set(MidwareName.domChange, (appName, mountNode) => {
+    mountNode.querySelectorAll('img').forEach(el => {
+      const src = el.getAttribute('src');
+      const { assetPublicPath } = apps.find(item => item.name === appName);
+      const newSrc = /^(https?\:)?\/\//.test(src) ? src : `${assetPublicPath}${src.charAt(0) === '/' ? src.slice(1) : src}`;
+      el.setAttribute('src', newSrc);
+    });
+  })
+  next();
+});
+
+use(theadOccMidware, { softCache: false });
+
+start();
